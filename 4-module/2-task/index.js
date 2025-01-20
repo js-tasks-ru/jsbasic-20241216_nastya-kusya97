d3d5e7e7ad0ad33ee3ad98b72d86d52a21055ae8
Anastasy Shushlebina <shushlebina@enterra-inc.com>
2025-01-20 21:58:37 +0700

    2nd task from 4 module

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,13 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  const rows = table.rows;
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    const cells = row.cells;
+
+    for (let j = 0; j < cells.length; j++) {
+      if (i === j) {
+        cells[j].style.backgroundColor = 'red';
+      }
+    }
+  }
 }
